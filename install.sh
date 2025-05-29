71d6213258b609d52d3e2a2cef38afd0febc079d
Mois Moshev <mois.moshev@bottleshipvfx.com>
2025-05-29 14:53:07 +0300

    Add dependencies as submodules

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-cp ./cairos_inst.json /opt/hfs20.5.510/packages/cairos_user.json
+# cp ./cairos_inst.json /opt/hfs20.5.510/packages/cairos_user.json
+python -m venv /usr/local/share/cairos/venvs/cairos
+source /usr/local/share/cairos/venvs/cairos/bin/activate
+
+pip install ./deps/cairos-types
+pip install ./deps/cairos-python-client
