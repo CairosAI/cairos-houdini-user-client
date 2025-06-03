6c3f42545a1eef8a8311a6a5928eb88be1ba379c
Mois Moshev <mois.moshev@bottleshipvfx.com>
2025-06-03 15:59:15 +0300

    Windows install script fixes

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,14 +1,14 @@
 $plugin_dest_default = "c:\Program Files\Common Files\houdini_plugins\cairos-houdini-user-client"
 $houdini_path_default = "c:\Program Files\Houdini20.5.510"
-$venv_path_default = Join-Path -Path $LocalAppData -ChildPath "\cairos\venvs\cairos"
+$venv_path_default = Join-Path -Path $env:LocalAppData -ChildPath "\cairos\venvs\cairos"
 
-$plugin_dest = Read-Host ("Plugin install location [${0}] " -f $plugin_dest_default)
+$plugin_dest = Read-Host "Plugin install location [${plugin_dest_default}] "
 if (-not $plugin_dest) { $plugin_dest = $plugin_dest_default }
 
-$houdini_path = Read-Host ("Houdini location [${0}] " -f $houdini_path_default)
+$houdini_path = Read-Host "Houdini location [${houdini_path_default}] "
 if (-not $houdini_path) { $houdini_path = $houdini_path_default }
 
-$venv_path = Read-Host ("Venv location [${0}] " -f $venv_path_default)
+$venv_path = Read-Host "Venv location [${venv_path_default}] "
 if (-not $venv_path) { $venv_path = $venv_path_default }
 
 $package_dest = Join-Path -Path $houdini_path -ChildPath "\packages\cairos_user.json"
@@ -17,7 +17,7 @@ Get-Content ./cairos_inst_windows.json | ForEach-Object {$_ -replace "{{ plugin_
 # cp ./cairos_inst.json $package_dest
 cp . $plugin_dest
 
-python 3.11 -m venv $venv_path
-$venv_path\Scripts\activate.ps1
+python -m venv $venv_path
+"$venv_path\Scripts\activate.ps1"
 
 pip install -r ./requirements.txt
